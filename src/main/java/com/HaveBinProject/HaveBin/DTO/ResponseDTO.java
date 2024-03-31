ffd273a289815aa8a60e28e4ed70d22093ced075
ChaGyoungtae <village1031@gmail.com>
2024-04-01 02:43:51 +0900

    2024-04-01 특정 위경도에 있는 쓰레기통 정보 보내주기 구현중

--- src/main/java/com/HaveBinProject/HaveBin/DTO/ResponseDTO.java
+++ src/main/java/com/HaveBinProject/HaveBin/DTO/ResponseDTO.java
@@ -3,15 +3,16 @@ package com.HaveBinProject.HaveBin.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseDTO {
     //화면 기준 왼쪽 밑 위경도 / 오른쪽 위 위경도
-    private Double maxLat;
-    private Double maxLon;
+    private Double neLat;
+    private Double neLon;
 
-    private Double minLat;
-    private Double minLon;
+    private Double swLat;
+    private Double swLon;
 }
