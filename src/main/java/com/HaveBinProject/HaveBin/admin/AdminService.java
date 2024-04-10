690efee2be7611a8031386951027c445d5648483
ChaGyoungtae <village1031@gmail.com>
2024-04-10 18:08:20 +0900

    0410 - 로그인 회원가입 완료

--- src/main/java/com/HaveBinProject/HaveBin/admin/AdminService.java
+++ src/main/java/com/HaveBinProject/HaveBin/admin/AdminService.java
@@ -1,15 +1,64 @@
 package com.HaveBinProject.HaveBin.admin;
 
+import com.HaveBinProject.HaveBin.DTO.RegisterTrashcanDTO;
+import com.HaveBinProject.HaveBin.Trashcan.Trashcan;
 import com.HaveBinProject.HaveBin.Trashcan.TrashcanRepository;
+import com.HaveBinProject.HaveBin.Trashcan.Unknown_Trashcan;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class AdminService {
 
-    private final TrashcanRepository trashcanRepository;
+    private final AdminRepository adminRepository;
+    public List<Unknown_Trashcan> findAll(){ return adminRepository.findAllUnknownTrashcan(); }
+
+    public ResponseEntity<?> acceptNewTrashcan(Long unknown_trashcan_id){
+        Trashcan trashcan = new Trashcan();
+        Unknown_Trashcan findUnknownTrashcan  = null;
+        try {
+            findUnknownTrashcan = adminRepository.findUnknownTrashcan(unknown_trashcan_id);
+        } catch (Exception e) {
+            ResponseEntity.badRequest().body("등록 실패");
+        }
+
+        trashcan.setLatitude(findUnknownTrashcan.getLatitude());
+        trashcan.setLongitude(findUnknownTrashcan.getLongitude());
+        trashcan.setRoadviewImgpath(findUnknownTrashcan.getRoadviewImgpath());
+        trashcan.setUserId(findUnknownTrashcan.getUserId());
+        trashcan.setCategories(findUnknownTrashcan.getCategories());
+        trashcan.setState(findUnknownTrashcan.getState());
+        trashcan.setDate(findUnknownTrashcan.getDate());
+
+        return ResponseEntity.ok(adminRepository.acceptNewTrashcan(trashcan));
+    }
+
+    public ResponseEntity<?> deleteUnknownTrashcan(Long unknownTrashcanId){
+
+        try{
+            adminRepository.delete_UnknownTrashcan(unknownTrashcanId);
+        } catch (Exception e) {
+            ResponseEntity.badRequest().body("삭제가 완료되지 않았습니다.");
+        }
+
+        return ResponseEntity.ok("삭제완료");
+    }
+
+
+    public ResponseEntity<?> deleteTrashcan(Long trashcanId){
+        try {
+            adminRepository.deleteTrashcan(trashcanId);
+        } catch (Exception e) {
+            ResponseEntity.badRequest().body("삭제 실패");
+        }
+        return ResponseEntity.ok("삭제완료");
+    }
 
 }
