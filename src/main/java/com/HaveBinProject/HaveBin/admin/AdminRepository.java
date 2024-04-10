eb0c7d8c8a7ee7d6332e4a836bc2dddf26a6119b
ChaGyoungtae <village1031@gmail.com>
2024-04-10 18:08:20 +0900

    0410 - 로그인 회원가입 완료

--- src/main/java/com/HaveBinProject/HaveBin/admin/AdminRepository.java
+++ src/main/java/com/HaveBinProject/HaveBin/admin/AdminRepository.java
@@ -1,9 +1,12 @@
 package com.HaveBinProject.HaveBin.admin;
 
 import com.HaveBinProject.HaveBin.Trashcan.Trashcan;
+import com.HaveBinProject.HaveBin.Trashcan.Unknown_Trashcan;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class AdminRepository {
@@ -11,5 +14,27 @@ public class AdminRepository {
     @PersistenceContext
     private EntityManager em;
 
+    public List<Unknown_Trashcan> findAllUnknownTrashcan(){
+        return em.createQuery("select ut from Unknown_Trashcan ut", Unknown_Trashcan.class).getResultList();
+    }
 
+    public Unknown_Trashcan findUnknownTrashcan(Long unknownTrashcanId){
+        return em.find(Unknown_Trashcan.class,unknownTrashcanId);
+    }
+
+    public Long acceptNewTrashcan(Trashcan trashcan){
+        em.persist(trashcan);
+        return trashcan.getId();
+    }
+
+    public void delete_UnknownTrashcan(Long unknownTrashcanId){
+        Unknown_Trashcan unknown_trashcan = em.find(Unknown_Trashcan.class, unknownTrashcanId);
+        em.remove(unknown_trashcan);
+    }
+
+    //id로 쓰레기통 삭제
+    public void deleteTrashcan(Long trashcanId){
+        Trashcan trashcan = em.find(Trashcan.class,trashcanId);
+        em.remove(trashcan);
+    }
 }
