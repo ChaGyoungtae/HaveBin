1f68a712669077d1a0eb9c4330e2ca9a594b85aa
HYEONSIK <ks06891@naver.com>
2024-07-18 18:35:44 +0900

    yml 파일 제거 및 health(상태) 코드 추가

--- HaveBin-main/src/main/java/com/HaveBinProject/HaveBin/RequestDTO/DuplicateEmailDTO.java
+++
@@ -1,8 +0,0 @@
-package com.HaveBinProject.HaveBin.RequestDTO;
-
-import lombok.Getter;
-
-@Getter
-public class DuplicateEmailDTO {
-    private String Email;
-}
--- HaveBin-main/src/main/java/com/HaveBinProject/HaveBin/RequestDTO/NicknameDTO.java
+++
@@ -1,9 +0,0 @@
-package com.HaveBinProject.HaveBin.RequestDTO;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class NicknameDTO {
-    private String nickname;
-}
